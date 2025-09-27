c633b7e15069ccde860beef7570acf8de5b25291
chick127 <coamos1110@gmail.com>
2025-09-27 17:50:51 +0900

    1st-1

--- src/app/dashboard-c/page.tsx
+++ src/app/dashboard-c/page.tsx
@@ -15,10 +15,10 @@ export default function DashboardCPage() {
   }
   return (
     <div>
-      <h1 className="text-2xl font-bold mb-5">Dashboard (client-side)</h1>
+      <h1 className="text-2xl font-bold mb-5">Dashboard (Client-side)</h1>
 
       <div className="mb-5">
-        <p>use client 지시자 사용함 </p>
+        <p>use client 지시자 사용 </p>
         <p>브라우저에서 useUser() 함수 이용</p>
         <p>Welcome, {user?.firstName}!</p>
         <p>Email: {user?.primaryEmailAddress?.emailAddress}</p>
