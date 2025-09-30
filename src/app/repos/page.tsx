e0e56cae7582023c9a5a138224aac0b7e79eecb7
chick127 <coamos1110@gmail.com>
2025-09-30 17:27:15 +0900

    1st-1

---
+++ src/app/repos/page.tsx
@@ -0,0 +1,48 @@
+import { Repository } from '@/types/repo'
+import Link from 'next/link'
+import React from 'react'
+import { FaCodeBranch, FaEye, FaStar } from 'react-icons/fa'
+
+const username = 'chick127'
+// const username = 'bradtraversy'
+
+export default async function ReposPage() {
+  const response = await fetch(`https://api.github.com/users/${username}/`)
+
+  await new Promise((resolve) => setTimeout(resolve, 1000))
+
+  const repos = await response.json()
+  console.log(repos)
+
+  return (
+    <div>
+      <h2 className="test 2x1 font-bold mb-4">
+        Github Repositories of {username}
+      </h2>
+      <ul>
+        {repos.map((repo: Repository) => (
+          <li key={repo.id} className="bg-gray-100 m-4 p-4 rounded-md">
+            <Link href={`/repos/${repo.name}`}>
+              <h3 className="text-xl font-bold">{repo.name}</h3>
+              <p>{repo.description}</p>
+              <div className="flex justify-between items-center">
+                <span className="flex items-center">
+                  <FaStar /> {repo.stargazers_count}
+                </span>
+                <span className="flex items-center">
+                  <FaCodeBranch /> {repo.forks_count}
+                </span>
+                <span className="flex items-center">
+                  <FaEye /> {repo.watchers_count}
+                </span>
+                <span className="flex items-center">
+                  <FaEye /> {repo.watchers_count}
+                </span>
+              </div>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
